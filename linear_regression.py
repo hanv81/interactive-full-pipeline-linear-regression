18d6c67221e2ffb8bef2a2f36a17150df5d53c8f
Viet Ha Nguyen <hanv.cotai@gmail.com>
2023-09-03 23:27:31 +0700

    training time

--- linear_regression.py
+++ linear_regression.py
@@ -1,8 +1,8 @@
+import time
 import streamlit as st
 import numpy as np
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -69,6 +69,7 @@ def draw_result(x, y, history):
   st.pyplot(fig)
 
 def fit(x, y, eta, epochs, batch_size=0):
+  t = time.time()
   w = np.random.rand(x.shape[1]+1)
   history = {'loss':[], 'weights':[]}
   x_ = np.concatenate((x, np.ones((x.shape[0], 1))), axis=1)
@@ -81,11 +82,12 @@ def fit(x, y, eta, epochs, batch_size=0):
     w, loss = gradient_descent(xx, yy, w, eta)
     history['loss'].append(loss)
     history['weights'].append(w)
-
+  t = (time.time() - t)*1000
+  st.write('Training time:', int(t), 'ms')
   return history
 
 def train(x, y, w0, b0):
-  col1, col2, col3 = st.columns(3)
+  col1, col2, col3, col4 = st.columns(4)
   with col1:
     eta = st.number_input('Learning Rate', value=.01, step=.01, max_value=.1, min_value=.0001)
   with col2:
@@ -93,6 +95,8 @@ def train(x, y, w0, b0):
   with col3:
     batch_train = st.toggle('Mini-Batch Training')
     batch_size = st.number_input('Batch Size', min_value=1, max_value=100, value=10, step=5)
+  with col4:
+    draw_loss = st.toggle('Draw Loss Surface')
   
   if not batch_train:
     batch_size = 0
@@ -104,7 +108,7 @@ def train(x, y, w0, b0):
     st.write('Weights by GD:', *history['weights'][-1])
   with st.spinner('Visualizing...'):
     draw_result(x,y,history)
-    visualize_loss_surface(x, y, w0, b0, history)
+    if draw_loss:visualize_loss_surface(x, y, w0, b0, history)
 
 def main():
   st.header('Linear Regression')
