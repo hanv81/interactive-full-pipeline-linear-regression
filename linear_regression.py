4e60992b2b6e32109e607d2250b5aa74ee944c7a
Viet Ha Nguyen <hanv.cotai@gmail.com>
2023-09-03 22:46:39 +0700

    visualize_loss_surface

--- linear_regression.py
+++ linear_regression.py
@@ -23,11 +23,12 @@ def visualize_loss_surface(x, y, w0, b0, history):
   loss = np.mean(((wb[:,0]*x + wb[:,1])-y)**2, axis=0)
 
   fig = go.Figure(data=[go.Surface(x=w, y=b, z=loss.reshape(ww.shape)),
-                        go.Scatter3d(x=w_[:,0], y=w_[:,1], z=loss_, mode='markers')])
+                        go.Scatter3d(x=w_[:,0], y=w_[:,1], z=loss_, mode='markers'),
+                        go.Scatter3d(x=w_[[0,-1],0], y=w_[[0,-1],1], z=loss_[[0,-1]], mode='markers')])
   st.plotly_chart(fig)
 
 def prepare_data():
-  n_samples = st.slider('Select Data Size', value=2000, min_value=100, max_value=20000, step=100)
+  n_samples = st.slider('Select Data Size', value=2000, min_value=100, max_value=10000, step=100)
   w = st.number_input('w', value=3.)
   b = st.number_input('b', value=2.)
   x,y = generate_data(n_samples, w, b)
@@ -49,9 +50,6 @@ def gradient_descent(x, y, w, eta):
   w = w-eta*dw
   return w, loss
 
-def generate_weights(n_features):
-  return np.random.rand(n_features+1)
-
 def draw_result(x, y, history):
   w = history['weights'][-1]
   x_line = np.array([x.min(), x.max()])
@@ -70,7 +68,7 @@ def draw_result(x, y, history):
   st.pyplot(fig)
 
 def fit(x, y, eta, epochs, batch_size=0):
-  w = generate_weights(x.shape[1])
+  w = np.random.rand(x.shape[1]+1)
   history = {'loss':[], 'weights':[]}
   x_ = np.concatenate((x, np.ones((x.shape[0], 1))), axis=1)
   for i in range(epochs):
